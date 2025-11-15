7de33f7ebd505817d5266b56e1f41fc2d4a2256d
FinuraBR <73367491+FinuraBR@users.noreply.github.com>
2025-11-15 17:10:15 -0300

    cansei
    eu quero q o service worker vá pra casa do krl

--- js/script.js
+++ js/script.js
@@ -66,14 +66,6 @@ document.addEventListener('DOMContentLoaded', () => {
         setupTooltipLogic();
     }
 
-    // --- REGISTRO DO SERVICE WORKER (para funcionalidade PWA/Offline) ---
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', () => {
-            navigator.serviceWorker.register('./service-worker.js')
-                .then(registration => console.log('Service Worker registrado com sucesso:', registration))
-                .catch(error => console.log('Falha ao registrar o Service Worker:', error));
-        });
-    }
 });
 
 
